4b40f9220b76efb1dd7bc0834e7ff83b49f87b60
GopalKharche10 <kharchegopal10@gmail.com>
2024-07-30 21:23:42 +0530

    Add files via upload

---
+++ 7_division.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+  int main()
+  {
+    printf("%d/%d=%d\n",9,2,9/2);  //4
+     printf("%d/%d=%d\n",9,-2,9/-2);//-4
+    printf("%d/%d=%d\n",-9,2,-9/2);//-4
+    printf("%d/%d=%d\n",-9,-2,-9/2);//-4
+        return 0;
+
+
+
+
+  }
