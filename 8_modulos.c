4d926efbb5a0a80b91edf4fbec3e162fb0204480
GopalKharche10 <kharchegopal10@gmail.com>
2024-07-30 21:23:55 +0530

    Add files via upload

---
+++ 8_modulos.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+  int main()
+  {
+    printf("%d%%%d=%d\n",9,2,9%2);  //1
+     printf("%d%%%d=%d\n",9,2,9%2);//1
+    printf("%d%%%d=%d\n",9,-2,9%-2);//1
+    printf("%d%%%d=%d\n",-9,-2,-9%-2);//-1
+        return 0;
+
+
+
+
+  }
