ca0cdc1d84977ffa70051f1ccefce822db2a393c
GopalKharche10 <kharchegopal10@gmail.com>
2024-08-09 13:03:45 +0530

    Add files via upload

---
+++ 16_sizeof.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main(void)
+{
+    int iNo=10;
+    char chchar='A';
+    float fNo =57.33;
+    double dNo=69.33;
+    
+    printf("%d",sizeof(int));//4
+     printf("%d",sizeof(char));//1
+         printf("%d",sizeof(float));//4
+             printf("%d",sizeof(double));//8
+
+        
+    printf("%d",sizeof(iNo));//4
+        printf("%d",sizeof(chchar));//1
+            printf("%d",sizeof(fNo));//4
+                printf("%d",sizeof(dNo));//8
+
+ printf("%d",sizeof(20));//4
+     printf("%d",sizeof('z'));1
+         printf("%d",sizeof(99.99));//8
+             printf("%d",sizeof(99.99f));//4
+
+
+ printf("%d",sizeof 10);//lu
+    printf("%d",sizeof(iNo));//4
+        printf("%d",sizeof(int));//4
+return 0;
+
+
+          
+
+
+
+            
+
+
+
+         
+
+
+
+
+}
