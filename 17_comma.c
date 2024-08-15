3641d94a2168acf1587b3e2805d44be9d55caf60
GopalKharche10 <kharchegopal10@gmail.com>
2024-08-15 12:30:28 +0530

    Add files via upload

---
+++ 17_comma.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int iAns1,iAns2;
+    iAns = 10,20,30;// selected 10 beacouse it binding with iAns//operators
+    iAns2 =(10,20,30);//selected 30 beacouse the hole bracket binding with iAns so we perform left to right operation on that brcket so 30 will be selected.//operators
+
+    printf("%d",iAns1,iAns2);//as we printf  %d so only iAns1 will be printed //sepraters
+    return -1,1,0;// operator
+
+}
